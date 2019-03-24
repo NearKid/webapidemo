fe01a6b952b7a075c0bf6b4ae94a0df63e767659
luch <1097650398@qq.com>
2019-03-24 23:17:19 +0800

    1097650398@qq.com

---
+++ WebApiDemo/WebApiDemo/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiDemo.Controllers
+{
+    /// <summary>
+    /// Values
+    /// </summary>
+    public class ValuesController : ApiController
+    {
+        /// <summary>
+        /// 列表请求
+        /// </summary>
+        /// <returns></returns>
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        /// <summary>
+        /// 翻个请求
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Post请求
+        /// </summary>
+        /// <param name="value"></param>
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Put请求
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        /// <summary>
+        /// Delete请求
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
